1edee3977157fafe9d93221522bf8b2d94a32b47
Alex Redfearn <alex.redfearn@dilaleredmac.mgsops.net>
2022-09-07 14:53:00 +0100

    Get input nd output file

--- Winner.cs
+++ Winner.cs
@@ -1,10 +1,60 @@
-﻿namespace Winner
+﻿using System;
+
+namespace Winner
 {
     class Winner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            string input = GetInputFile(args);
+            string ouptut = GetOutputFile(args);
+
+            Console.WriteLine(input);
+            Console.WriteLine(ouptut);
+
+            if(input == "")
+            {
+                Console.WriteLine("Missing input file");
+            }
+
+            if(ouptut == "")
+            {
+                Console.WriteLine("Missing output file");
+            }
+            
+        }
+
+        private static string GetInputFile(string[] args)
+        {
+            return GetFile("--in", args);
+        }
+
+        private static string GetOutputFile(string[] args)
+        {
+            return GetFile("--out", args);
+        }
+
+        private static string GetFile(string option, string[] args)
+        {
+            try
+            {
+                if(args[0] == option)
+                {
+                    return args[1];
+                }
+            
+                if(args[2] == option)
+                {
+                    return args[3];
+                }
+
+                return "";
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception caught: {0}.", e);
+                return "";
+            }
         }
     }
 }
